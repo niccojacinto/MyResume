fdf6931a30b884d219d9bea442a54ec9b88b79d2
Nicco Jacinto <nix_jacinto25@hotmail.com>
2023-08-30 07:17:01 -0400

    fix xmlloading for webgl builds caused by not removing leading byte order mark

--- Assets/XMLLoader.cs
+++ Assets/XMLLoader.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.Networking;
 using System.Xml;
 using System.IO;
+using System.Text;
 
 public class XMLLoader : MonoBehaviour
 {
@@ -24,49 +25,54 @@ public class XMLLoader : MonoBehaviour
 
     private IEnumerator LoadXML(string path)
     {
-        string xmlPath = Path.Combine(Application.streamingAssetsPath, "MyResume.xml");
-
         using (UnityWebRequest www = UnityWebRequest.Get(path))
         {
             yield return www.SendWebRequest();
 
             if (www.result == UnityWebRequest.Result.Success)
             {
-                string xmlData = www.downloadHandler.text;
-                XmlDocument xmlDoc = new XmlDocument();
+                //string xmlData = www.downloadHandler.text;
+                //xmlData = xmlData.Trim();
+    
+                byte[] bytes = www.downloadHandler.data;
+                string xmlData = Encoding.UTF8.GetString(bytes).TrimStart('\uFEFF');
+
+                Debug.Log(xmlData);
                 try
                 {
+                    XmlDocument xmlDoc = new XmlDocument();
                     xmlDoc.LoadXml(xmlData);
-                    // Now xmlDoc contains the parsed XML data
+
+                    XmlNode root = xmlDoc.DocumentElement; // Root element
+                    XmlNodeList dialogues = root.SelectNodes("Dialogue"); // Select nodes by tag name
+
+                    foreach (XmlNode dialogue in dialogues)
+                    {
+                        Dialogue d = new Dialogue();
+                        d.id = dialogue.Attributes["id"].Value;
+                        d.speaker = dialogue.Attributes["speaker"].Value;
+                        d.next_id = dialogue.Attributes["next_id"].Value;
+                        d.image_id = dialogue.Attributes["image_id"].Value;
+                        d.image_pos = dialogue.Attributes["image_pos"].Value;
+                        d.dialogue = dialogue.InnerText;
+
+                        XmlNodeList responses = dialogue.SelectNodes("Response");
+                        d.responses = new List<(string, string)>();
+                        foreach (XmlNode response in responses)
+                        {
+                            d.responses.Add((response.Attributes["next_id"].Value, response.Attributes["text"].Value));
+                        }
+
+                        // Debug.Log($"Convo ID: {d.id}, Speaker: {d.speaker}, Dialogue: {d.dialogue }");
+
+                        DialogueManager.Instance.AddDialogue(d.id, d);
+                    }
                 }
                 catch (XmlException e)
                 {
                     Debug.LogError("Error parsing XML: " + e.Message);
                 }
-                XmlNode root = xmlDoc.DocumentElement; // Root element
-                XmlNodeList dialogues = root.SelectNodes("Dialogue"); // Select nodes by tag name
 
-                foreach (XmlNode dialogue in dialogues)
-                {
-                    Dialogue d = new Dialogue();
-                    d.id = dialogue.Attributes["id"].Value;
-                    d.speaker = dialogue.Attributes["speaker"].Value;
-                    d.next_id = dialogue.Attributes["next_id"].Value;
-                    d.image_id = dialogue.Attributes["image_id"].Value;
-                    d.image_pos = dialogue.Attributes["image_pos"].Value;
-                    d.dialogue = dialogue.InnerText;
-
-                    XmlNodeList responses = dialogue.SelectNodes("Response");
-                    d.responses = new List<(string, string)>();
-                    foreach (XmlNode response in responses)
-                    {
-                        d.responses.Add((response.Attributes["next_id"].Value, response.Attributes["text"].Value));
-                    }
-
-                    // Debug.Log($"Convo ID: {d.id}, Speaker: {d.speaker}, Dialogue: {d.dialogue }");
-
-                    DialogueManager.Instance.AddDialogue(d.id, d);
-                }
             }
             else
             {
@@ -84,6 +90,7 @@ public class XMLLoader : MonoBehaviour
             // Handle WebGL-specific loading (e.g., using UnityWebRequest)
             // You might need to adjust how you load the file due to WebGL security restrictions.
             StartCoroutine(LoadXML(xmlPath));
+            Debug.Log(xmlPath);
         }
         else
         {
